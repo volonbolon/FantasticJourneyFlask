c1d96be41f84ba97c44b6e765eeeb49465bf81db
Ariel Rodriguez <ariel@volonbolon.me>
2017-09-23 18:54:14 -0300

    Add gitignore

--- FantasticJourneyFlask.py
+++ FantasticJourneyFlask.py
@@ -4,9 +4,21 @@ from flask import request
 from flask import make_response
 from flask import jsonify
 from flask_pymongo import PyMongo
+from datetime import datetime
 
 app = Flask(__name__)
 mongo = PyMongo(app)
+
+@app.route('/')
+def homepage():
+    time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400">
+    """.format(time=the_time)
 
 @app.route('/locations', methods=['POST'])
 def locations():
@@ -30,4 +42,4 @@ def locations():
     return response
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
